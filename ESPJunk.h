1310f96b3bde47c6570a104b92028ec28ee356c8
danielkrupinski <danielkrupinski@outlook.com>
2017-12-22 17:02:23 +0100

    Add junk code to make cheat VAC undetected

---
+++ ESPJunk.h
@@ -0,0 +1,528 @@
+#pragma once
+
+//Random generated junk code for no VAC detection
+
+	void SDMZZpSzSGYbCsFtBHiAbqVBDqa() {
+		float IOgFDVxQ = 4.347739E-19F;
+		int sVKuIZdUlxQcYUC = 22948963;
+		int RSyYxiDO = 9326;
+		short sWzZlYdHJuPFpGVZeLSDjaHMJIy = -24631;
+		double pCZQNZNtLEtLkKfTEwcDQYen = 1.332383E-08;
+		byte PElagwHiCZcQC = 80;
+		byte YYSKGVgDySSXSuEyUWlYPoPCbxg = 31;
+		byte uoIZfPdpQUxJDnDKp = 31;
+		float WGdffKiqJiOaCWzDoS = -9.792909E+20F;
+		float gsDgMBZsfaLxGPkDTx = 8.905393E+18F;
+		long Npfp = 35586641010733130;
+		double gZfKZWIjc = -9.937162E-19;
+		double iiARkdSfXZKPVADceITGRIWQEKn = -2.803156E+13;
+		double XikbdmtyItOMZXPWdLSF = 1.910067E+24;
+		short nQAHZDpPnMTBedEfWOEGp = 24547;
+		byte iwE = 217;
+		long uAe = 6017109043511366;
+		int GaegFspk = 29;
+		int jwJgPFobyi = 910191;
+		if (jwJgPFobyi == 949204) {
+			jwJgPFobyi = 808301;
+		}short jbHllDBGei = -5247;
+		float eXfzkYtHmaoxpaFeIogCcJHSNPFHk = 1.10457E+17F;
+		byte JTkagYDMZkHpSoCFBjAxTVwq = 67;
+		double bKKl = 6.012832E+33;
+
+	}void dip() {
+		int utuzwSOpzxquzH = 22;
+		if (utuzwSOpzxquzH == 564185) {
+			utuzwSOpzxquzH += 321822;
+		}double nWzEEASoiORosVSTcsL = 3.677114E+12;
+		byte sEQuTkdxmASlOKlYxeGCphRXpTh = 60;
+		long taQsHbKBGjMSeZdRzRxVLhSBGzV = 50445969401925585;
+		short FOanaaIZCTjXpM = 13772;
+		byte klipNigffBEsj = 178;
+		double wTcwILgjCeFLHetVsninYgSGUhUB = -1.458453E+10;
+		float XuVMwK = -0.004728027F;
+		long GgBWxDFGEMCnaAzAVbPVb = 21367539121845695;
+		byte AYNiJqRnJIuJqJRAUR = 36;
+		long aqRxtwLHfVTLHJfODPbKfUwXgy = 33088800147965844;
+		byte xlJ = 195;
+		short TpWshOftHgQKMUGfptgweSlmSgR = 30050;
+		short VdT = -2113;
+		byte NtijtYzOuBLgwpWpHLR = 4;
+		byte AaXhnGkNQnUZtWjXcguzS = 85;
+		float DPeGxiHZDdUhmkKTEILym = -8.678911E+32F;
+		byte JJLHFR = 39;
+		long bLRx = 80853380684215834;
+		short NBWtYlMEZFlsYqdQ = -9521;
+		double pzLlj = -2.15542E+32;
+		int KAQCdEsAPmBAPmIkazdE = 287;
+		if (KAQCdEsAPmBAPmIkazdE == 722954) {
+			KAQCdEsAPmBAPmIkazdE += 486279;
+		}double MLquxJKPSluzVMLSabqqE = 1.976588E-33;
+		float BSyOJpQDamtmiuEKNiLVuxzsdc = 0.00168483F;
+		int CAxBAdtfKHFsTxPdTIHyB = 926381;
+		int mSdkZdYSTcKNdA = 7731948;
+		float sDnWssYnfmhamkz = -8.440485E-22F;
+
+	}void tfkwJiTQdBdHQPcghsZjQgAyXZ() {
+		int UJfKSieomXUedCnykAytYgc = 37;
+		int CVOOGZpRfRJAAki = 625317285;
+		if (CVOOGZpRfRJAAki == 831351) {
+			CVOOGZpRfRJAAki += 711642;
+		}short blwZyliEE = -21399;
+		float uUmhiY = -1.65945E-24F;
+		double ZtpWukgTd = -0.001622911;
+		double utZJDTUYFCYXdZcICqaNlag = -0.3509066;
+		long nnecOPmnw = 76583791994706665;
+		int Cfd = 68245478;
+		short FalAPZKzGzFVEdURXdHb = -3107;
+		short NfjAwaXLaiNpeeZxbuEHDWkA = 13248;
+		float BnyINFMROQNYHFIUCmQ = 1.784542E+15F;
+		double bSHuiqsJlZTWgW = 0.000151547;
+		short WyislLzgQpJajJZImHFZneIX = -6200;
+		double REQTWgjeSNaWnHjTx = -1.203528E-18;
+		float luNERgOdLAlm = 6.014846E-05F;
+		short bnmPKZMBIk = 11912;
+		long EouslMZRbpkSGEAno = 33692677220880979;
+		byte OPebRGluwbwLPQgTiSXBVHMwZQj = 109;
+		double EBahcKbbxPzNZFBZF = -1.059718E+33;
+		int yeFqHzupByyfiX = 25;
+		float QiLDyAmbikCZKBzSMWjqhhFyLj = 6.078411E+12F;
+
+	}void BBhMDubfu() {
+		float mkYHmdTZfJqMYiyYBbtlGZqNqfl = -2.695084E+11F;
+		long kdhtyIHhebfEEiOiDe = 85934887681608034;
+		int hSSsNfgJRQ = 33;
+		if (hSSsNfgJRQ == 265789) {
+			hSSsNfgJRQ = hSSsNfgJRQ + 203572;
+		}double NEEWJVlRzjVSfD = -3.272472E-13;
+		int ZNuBQW = 475;
+		double BXfLwGbPEPlFWTJTVY = 2.38759E-28;
+		long VZdExLTgdsqFnucMhJyATwkfRh = 89574720150236867;
+		long ZNuLSTHfVmOMXSQp = 3065327766941811;
+		long silCjdbklkZTFFzoQwx = 855397486341539;
+		float IMRgpYBnOnDsxxidsUiuRVDmyPPz = -3.209027E+17F;
+		byte NdeNiEJBjccbJxJAKJCTjUEHK = 236;
+		long UhHSAzcmNpuxH = 38161734913538079;
+		double XPWWwaVDiCgtXekBQ = 1.274198E-28;
+		long MaLahPwTFo = 9963497558770941;
+		byte KfQiIpCVDyUWchJl = 198;
+		float zlsDNqDLBcxTfCQGGjCtSmBp = -1.072973E-08F;
+		float DcbmaYkLb = -6.742267E-13F;
+		long phMgjWDtEDcyKoZVDN = 80343020092589304;
+		short YWmtFZYVuDcqKo = 7466;
+		byte OLlbGdcJuByoYIJPfu = 161;
+		long SLpdBZKuoOlSjLQVwtTjK = 3618714382116005;
+		double epTLT = -8.052008E-28;
+
+	}void RmAXMnZiBTRj() {
+		short UABGcXZjIntqcMVZWTxbBGesiapN = 505;
+		short bdOJlGpOGIJFU = 17803;
+		float lsJyGuexowixbImKBSsnn = -1.262569E+14F;
+		float eQuSbjEMqduSJPIZquR = -3.732244E+28F;
+		short YMUyBPsgh = 14185;
+		int JAtXjLgsEZPEuc = 7037;
+		short lYDnFSmlZISbznpifoInwlNKKAb = 16984;
+		byte xaqOmuwWfofsahLtEbumMYCRAZl = 218;
+		int DtazGiX = 864512786;
+		if (DtazGiX == 10267) {
+			DtazGiX = DtazGiX + 458263;
+		}double OiIwPGUljQguTOtkx = 7.437782E+19;
+		long HUFLRoKTdxfSZAMfmbauquhqzeZd = 28762411464230364;
+		byte wPypzjfQNMoPRaNYGlMm = 59;
+		float AQPuQGjeRkNkuCbFpObEu = -1.433146E+26F;
+		float zOV = 1.336688E+08F;
+		byte ygq = 17;
+		long JBlfqMKqUmiMPypmaAfVYsbCQ = 2068217059733491;
+		byte sCXB = 130;
+		long TfblBuyaZCFGLuSRZHqmZg = 53330869687681004;
+		byte UuYSsHQe = 59;
+		long dQVtIBLXUjcMNNDa = 38977247355695149;
+		long aztQlbLHoJpwXl = 30336228381052132;
+
+	}void saCUTPGFYgPSSgqgcgmPTYBAN() {
+		byte lTYKfOAOcn = 185;
+		int opPplCqmLhqVtOYuBcgQBuMZyK = 36;
+		int wtUFQSDyZUwoaZYnwkp = 90921524;
+		long WeRYNHMAaGZssKONgfxHP = 68637512334019848;
+		short qNEMUisKQCkb = -20822;
+		short wUidwFlyOqoQdLpnqD = 30409;
+		long eaYPbLOfoPwGtnZBmUAkEnuiU = 24768265077050861;
+		double IuoniNFIafn = -5.341957E+25;
+		double GFWaRFazcYlZsaRZmZH = 6.358647E-17;
+		double nDgKdgaxJUijbCQLXOaGlMM = -6.135717E+35;
+		float pSyyEFMCwWSTbkyTpZoFlmw = -1.882769E+17F;
+		long oqbCKUWfYxtEuKCqh = 88339285715473987;
+		long XlGpaTQgkGcqpFCUFtTJd = 86044795110214179;
+		double FIbWxTAOlGV = -6040.9;
+		long ZLccuEDzsmFbZiUotadHTMHy = 89722505620397392;
+		short neWJbpPZhnhdydemgWg = -6504;
+		double ZPGYPlClPONlbRUR = 1.820855E-16;
+		byte LPHceoMMOSPUBjDpUPgUzl = 251;
+		long HtgdJeUzNlLNngNBGCicdZ = 41418284601353489;
+		double qqyVGJfNUwOYSEGIttSKRmJI = -1.119802E+16;
+		double aHdlFFsBDEYItNOmLzlHjmbOteuz = -1.182964E+20;
+		byte oUiDTaSIbXA = 0;
+		double XVzXkoZzBnmMsB = 2.092413E-29;
+		long YcCXJXG = 51921481215813034;
+		long ZdLNVfq = 53575581133373122;
+		byte wuuuFZIpheHlqecnCCHHwdWjABx = 180;
+		double UdDXxoWq = 2.831517E+17;
+
+	}void RQaAWSQeOFpmoGoboDbN() {
+		long ujpzP = 25211556882565518;
+		long mMkyKw = 5344073789293361;
+		long UpiRiRowWJpyyCdfTqzZaFHJoOyNl = 24594353437033697;
+		double xxQCbeIYqIxWhzpXoKACwtDjqIRX = 2.262354E-28;
+		byte CSosoZabwWUgmDfKVhTn = 146;
+		int KPuqJHQbZkRLqAdglcACpkBLd = 9468;
+		if (KPuqJHQbZkRLqAdglcACpkBLd == 461575) {
+			KPuqJHQbZkRLqAdglcACpkBLd = 734948;
+		}double JmtKfFCzWg = -4.207807E+09;
+		short hInYtLHj = -19342;
+		byte yzAdUPwWgDqOPJaYfFfOMIiW = 170;
+		double byjIdkEgzukOW = 1796578;
+		long KEVThm = 68004677118412467;
+		short aqYSZzTzaZjK = -9168;
+		short oXHZDTgySj = 13685;
+		short HKAnZaWewOinh = 4495;
+		long CLpAYiDj = 48201020843748060;
+
+	}void KQHpunOGlMXDh() {
+		int NtGOZ = 85894702;
+		short JpThqDxltdqT = -5564;
+		double iRQiNiYbqMYfZhPdI = 3.382319E+22;
+		byte SeEzGZBOhwjFtZ = 74;
+		long FEqHCRuxSxohsdBbgOi = 5408500436460462;
+		short HYs = -18689;
+		int udpgHd = 451706446;
+		long yPSnUPjIDOmihfwRYRMLf = 50092286583114346;
+		short iWuKIQUIDk = 20633;
+		short mtunJcHNDxmhww = -20567;
+		double tTQKaBlstdgDtngMzujRDkqXhexoA = -1.830355E+20;
+		short xmLwJnnVOxdWyFbqZEBxZQnhDQe = 11845;
+		byte PNfbIQbxoRjFjOsBgRDWkYuFo = 47;
+		double zUbGMBCgCKzLzQxqJLtVLP = -26774.87;
+		int dtyc = 29961238;
+		if (dtyc == 87528) {
+			dtyc = 296427;
+		}double MRnoXIVhFEEdFEoRls = -3.075206E-07;
+		long jKnKUSENIOcIAhtoNR = 26990729110713702;
+		double HXooCiQgwhZWSzKdUj = -8.137983E+32;
+		long lEDuSPeFT = 86223072433204294;
+		double pMMlqGoqchwGNEj = 0.01446315;
+
+	}void aBMVIbTMZiRJsiFtblT() {
+		short QRxYZPOpfgBJVAgKpwBUdPqQ = 18747;
+		short TCmgiSqD = 16903;
+		long EXtRwZfjofweaugbcxdxGHCml = 18260007812348208;
+		short VzbVzm = -9143;
+		short KhFMq = -20686;
+		byte gIFLIMxcLhXQeVwUotnTlLLfNZst = 65;
+		byte HDnLTxmxyoulTUnUVm = 42;
+		short HiNdnYXhAfxddUMkHEQQnzy = -13776;
+		double ISVcTEulWflydGmJnln = 2.3117E-36;
+		int mfVwitlG = 36;
+		double pUPTqpoWOERLfpfDnsjpUsK = 3.806556E-19;
+		long cLNOHUmxoaFlFWNp = 20804803720984228;
+		long HuiMfmuFU = 55794336597267443;
+		byte QuESFbSJFljsPwumSmD = 229;
+		long acpjwWBYlaqsERdDFLnEQsKfp = 43679700874521796;
+		int JSHObJskTsF = 989;
+		double smMFJNtJKQaHJPcoA = 1.636815E+13;
+		byte SNYuVSEwYYwhMzKEqZan = 48;
+
+	}void TPCdKBybkLdiXhfItuo() {
+		byte pBRitELmCO = 217;
+		short DkFfePVoUUzMViNUKPcEXZFLj = -29059;
+		double eDliEgUSVgIVaqCuq = 1.527612E+07;
+		float SNODuVuZLitpTfnVYFswU = 7.926537E+11F;
+		short ElOBbXMV = -19346;
+		long JGzJkHuJxCaUKWaAKuaFCAbNK = 67634120237430430;
+		byte QWpdDlspkNyajboALFNojFL = 249;
+		float ZNLMM = 2.315371E+11F;
+		short XbkSGYsbjWHdQAYZRuawVDDEJEz = 94;
+		int bjnNNUkHMZwzHicBSxPaDeq = 95;
+		if (bjnNNUkHMZwzHicBSxPaDeq == 977630) {
+			bjnNNUkHMZwzHicBSxPaDeq += 162617;
+		}byte qsPHtkOTBLxmzYCxutsBIxjlW = 217;
+		short RGlzKhdfSCJpeEopgSTgxAU = 7046;
+		short uRipDmbVcVPgfNmzH = 2154;
+		byte KiRpusLypbEPoDOdXOyO = 80;
+		float MTbSyLQkGjyRkZSVjCEGa = -5.548037E+37F;
+		double mRgjtwZRxfxZRcFzSLDij = 6.620978E-23;
+		int lRRwwSoBsnRTHQ = 34;
+		if (lRRwwSoBsnRTHQ == 4294) {
+			lRRwwSoBsnRTHQ += 682653;
+		}double qndSsfNPjXFbJmxF = -2.276208E+32;
+		int DPhJzGlMhG = 2;
+
+	}void wDtmfR() {
+		short dKjPAwOI = 13240;
+		short JdR = -31332;
+		float ZzuVbWJjwtuGgZJAqymSQhWfYSJyN = 9.88096E+23F;
+		float FhSCoWinkyLDLoKwIXbEkna = 9.820944E-40F;
+		int lfbD = 22613;
+		if (lfbD == 394097) {
+			lfbD += 494462;
+		}long yDGxP = 28593341535229344;
+		double IcVfwTDDhWwypBRnUBexAJsugaCz = 4.133504E-36;
+		short fmplmhzYMJI = 27956;
+		byte BIPwJWCtkBDqPuFKxkiSO = 190;
+		int lxJswKMGkzX = 9331;
+		int jXCOWfqynN = 23;
+		short EftNMdqKl = 3706;
+		short WRgGlVzxRgMZwbRyRqUWwu = 15234;
+		double csdXzu = 1.150022E+07;
+		long dLKKpV = 3091049175306280;
+		short RTRRifDxaPI = -5288;
+		long ptttBlwQwhKnJb = 32460032099962169;
+		int JBJVboYMYuEzyQFqajuA = 72920424;
+		
+	}void PfjXpozmbhtRnBgO() {
+		long xIKTobPcNfTyhBU = 25103086464424587;
+		short YsH = -29074;
+		short PapaCJLTAWtdgPU = 8636;
+		int kVlu = 43;
+		long haHIexSBtRPSFmJZnshqe = 28991629059778092;
+		long TCQOt = 76735269336618886;
+		short PTNuYkKZFFiyx = 13061;
+		double TeMWpOApDQIJDqTyeSFbfmoUiDljk = 2.990216E-36;
+		long WnLRyXzQDUkjJC = 15810523972921778;
+		long gyXAzeDpfLtTugbCiHqCMfho = 62039193970407336;
+		long MHjWzxbCjoTjScigcsy = 62996032280640193;
+		double noquXpiUYlmmXcQslNdW = -3.280195E-05;
+		byte LtDCDopVYVaBxQUQRmFHYZDmhI = 232;
+		long gnQkRUQNWXfqLufRBuzTZJioy = 66259329867520537;
+		byte IjoFWdcUozpnGywHmywTyeCOX = 40;
+		byte DcMpHDyIlQDPWPT = 206;
+		short gtUNJWGktEICHOdFsmN = 31620;
+		double YnmTfOKnfVFlFJyuXoxyiNMgAC = -6.786944E+22;
+		float SyUjmpIdMMfmhnNdQnaVQdJB = 1.119825E-08F;
+		float SMJnLQxVVTsDqUTGaJR = -6.560458E-06F;
+		short DKfZlQnGGhUCwHCKaamqUeED = -31198;
+		int gKGsifIfXzffmYRaJzcSClqO = 55;
+		if (gKGsifIfXzffmYRaJzcSClqO == 985310) {
+			gKGsifIfXzffmYRaJzcSClqO = 459980;
+		}double kAZWJKEBFDYpzwBXaABGdTpZhPz = 1.499375E-13;
+
+	}void LARDfRUFcADDOWuNLtjpp() {
+		short TCLUIqjYjJBPUMqwu = 3448;
+		double uXP = 8509.46;
+		double dfIfggLkoVPyRhhq = 2.458198E-26;
+		byte CJkYeAEPyoNiVFXETzBquHVDP = 15;
+		double ITbxGYPZakqpGenYGFSD = 4.256002E+09;
+		float MjGSRasJmIionHYcWkUwpwJezuyT = 2.014304E+25F;
+		int BPoSZGjZPUEc = 53228110;
+		if (BPoSZGjZPUEc == 120873) {
+			BPoSZGjZPUEc = 880109;
+		}float jcBJbIxJQTZ = 5.618345E-36F;
+		short cMpFfpiWXPpJXABksHJGMhkx = -939;
+		double fBqpbVlQfEFhnYiGTMlDMYpEmc = -4.6099E+20;
+		int mSyOlgTEGOQyMikqbsUUh = 148859;
+		int OndNkmxsCBSdPLhnm = 54;
+		if (OndNkmxsCBSdPLhnm == 764795) {
+			OndNkmxsCBSdPLhnm += 811569;
+		}float WsRXqYyMJVEXgqXeWLjsFpN = 6125.897F;
+		byte liVBHqmBBPkxNlnHBTGoJBn = 189;
+		byte ahGGlup = 55;
+		int OwWHUQyoLoxgpJlMXN = 72;
+		if (OwWHUQyoLoxgpJlMXN == 494790) {
+			OwWHUQyoLoxgpJlMXN += 478776;
+		}long UUDRg = 18809011545425253;
+		long JVgONxmX = 49784038908264582;
+		int KFXAnhESPlglKBk = 806099207;
+		if (KFXAnhESPlglKBk == 147881) {
+			KFXAnhESPlglKBk = 708682;
+		}
+	}void DttkaaHU() {
+		double MnaMqxsVtOPNyVdhOnUhWLpatn = 1.634338E-28;
+		double zUcRzeZnnGepwLgGULIupmmRj = 2.843052E+23;
+		short fAwfUZ = -6194;
+		int dofYECyZk = 8990;
+		long bQnTQxUzMEeObeiQdGyI = 53852677929948121;
+		float KLgZcNYtn = 3.108932E-22F;
+		double YskjjMIzRWLPkceNwiLdzUtIb = -4.453022E-07;
+		float NGyIPI = 9.877013E-20F;
+		short ctqoYGUKazsBGCuAgifuI = 12314;
+		short eFmpo = 21276;
+		int PXJAaOXCcsqYPmZWmpBy = 74;
+		int sEujjKMISVzt = 5829;
+		long GqcYpRBcWpnNDFjzXgOeCqdsR = 40288330769954473;
+		float bCwUtjJMCHMQHuTslwkpnmL = -3.696873E-26F;
+		short CIahpugnCHKQfpsiLPWHDQFzES = -29402;
+		double PCxtGJDwHSgkTzpMcDEzsobIT = -4.762906E+27;
+		long GMAflTkOSIPRbGSJ = 12581696714141760;
+		float gaMDHioPNekDXRTTcKVDgcgkfyR = 4.829811E+11F;
+		double SoifTmdTnTSgdRxSVVBNbGzXze = -8.439336E-19;
+		double VKgDdMKmmZA = -1.062261E-05;
+		int mWkSDQJPdTNBOQesG = 932761;
+		if (mWkSDQJPdTNBOQesG == 464994) {
+			mWkSDQJPdTNBOQesG = 38659;
+		}byte UBaBXypmDzeoLBBiwZwVpWPI = 231;
+
+	}void xmLwJbyq() {
+		int XAHZhAXLsGObcCgPcStQiL = 18460226;
+		long dliCOVccoRNGeTkXm = 49777441417791821;
+		int GDCfFsPDCTCNlNaDDd = 613836;
+		short HoyUiDlTpBPRVppMfuI = -11504;
+		short CbHd = -12688;
+		long BJPgQPy = 34010522303711735;
+		long hEDaSntnlHYtqskfny = 60839278325478988;
+		long wlywYjQ = 53515677421549300;
+		short zBOgupajdKbZPPO = 32060;
+		int ajEwgDepfOdPbDxoMEBbbWxkVB = 13927285;
+		int EYVculbeklgnIADAcjAiSdNtBHPXn = 86445466;
+		if (EYVculbeklgnIADAcjAiSdNtBHPXn == 60523) {
+			EYVculbeklgnIADAcjAiSdNtBHPXn = 977682;
+		}double KjoZWMkdHTnFC = -1.404187E+16;
+		float WZYELmUQpJdGFKzd = 9.656059E+10F;
+		float JWmFJudlFshbRIUqJuEaL = -20.33882F;
+		int NNdtfqcELRaMtVqiJlYBcP = 794822055;
+		float AwaTVHaSzbfzgRTFoAlRcXDLKYRnl = 9.729271E-36F;
+		short OXuFJfWDgPRFWCSojXQNHOYh = -12428;
+		float mTPBCJljGqfpLsKE = -2.444687E-29F;
+		int YmcRlPuwSpoiBNtihaAoXDZQRppSB = 42;
+		long TLSiMBnxnVkM = 45371234202180989;
+		long nZhJmwuOmQfqVXhP = 55473801303428450;
+
+	}void dEtptiSB() {
+		float CpU = -6.867698E+33F;
+		float kgoXkD = -2.528392E-37F;
+		byte RNqnAw = 71;
+		int cKNGPLkobJaLGsqifunSak = 3432;
+		double langsawkwaetbasK = 8.368898E-08;
+		short VXhRbtsOotqnPiCLUiU = 24498;
+		short OIwFlF = 10450;
+		int PYlOmzOjBxxgOgIUg = 100413278;
+		int igA = 607524475;
+		long VhdeMmZHL = 3138857732153491;
+		int AcKhkYUgu = 6771;
+		if (AcKhkYUgu == 812) {
+			AcKhkYUgu = AcKhkYUgu + 701117;
+		}int wIjIankknnJTwkzRgPik = 877011451;
+		long YaQ = 15012788206614172;
+		long wVjlmyUniRTODpuL = 16634487983270602;
+		long KnqYQlqtjVgsGFhMHPHtuY = 23299395559987702;
+		int wpmhoVQcAzdKfjXmW = 434451334;
+		byte RniLEzJkJLqxqR = 68;
+		double bnZTWC = -4.683092E+13;
+		float QnQKPLqUtppAyMBnsnyR = 2.855161E+30F;
+		double IlaKUPAeGUKiDADuGdUxAhmkVb = 3.760857E-38;
+		float ghYlWdMPhfFApe = 37.81602F;
+		int yptC = 696633;
+		short KSdSVmEQN = 16010;
+		short AdoDmnklQdDiIcZxCM = -32022;
+		long RXVEaZtknesWbgkViLoNUwGoV = 46639459141192681;
+
+	}void qGIQfedaeGdpzd() {
+		double zaUSTHe = 38964.12;
+		short sNAymANINxpJILfmqiZgByOZyNm = -3039;
+		int BSBkjXbDaJTQ = 6877;
+		short BTTZuVZBaAmpywWzh = -3167;
+		double kOipnLf = -2.35995E+14;
+		float qnQNPFAdJasFFYf = -3.038766E+20F;
+		int YtGFeFTyCiYoNiPDcUbifsNtwxXp = 56;
+		if (YtGFeFTyCiYoNiPDcUbifsNtwxXp == 42474) {
+			YtGFeFTyCiYoNiPDcUbifsNtwxXp = YtGFeFTyCiYoNiPDcUbifsNtwxXp + 499263;
+		}short KXPgxqOSTVIwnugl = 12028;
+		byte HHljJIDtYyZmmaoTffUNPFVBB = 198;
+		double KxAo = 5.033601E+11;
+		int fRBXyCfF = 6584;
+		int cCFAeyyGmBcMngiskWGmLxVM = 83;
+		if (cCFAeyyGmBcMngiskWGmLxVM == 757704) {
+			cCFAeyyGmBcMngiskWGmLxVM = cCFAeyyGmBcMngiskWGmLxVM + 908996;
+		}long xqqWHPgJuULXzu = 55444666361585413;
+		byte GTpmzMSqlIpWQmLxQWT = 211;
+		short lVIGCL = 5250;
+		double koqUJfXXYWMkPsmxLiIahPw = -3.810159E-17;
+		double FeQakqSpTWsE = -1.307925E-14;
+		float NdDVgaMqjsgiFkxKNQKEoY = 114.7155F;
+		double GQqUHNCHnjgIeugzaSxOsbiVy = 3.6587E+16;
+		double ctdCfxghNY = -1.525584E+31;
+		float DhLZZCEzkoWx = 0.3209134F;
+		long iTUux = 69425994213978419;
+		long IIgIsLqoiRyqgVJK = 80874537684398909;
+		double wuyWfEDJufSf = 4.009409E-10;
+		short TBJdUufRl = 9898;
+		long yHVZOecJcDtZAjSdJKytexzbyu = 57649125547316090;
+
+	}void PFQfomCMjGkcIAKXPxV() {
+		float htHoMFHEcATFifDLnxmsAwFzykzcG = 1.210424E+15F;
+		long pfheUTGwdxkeknQmpTzocz = 22720766542617994;
+		float FTSawGuDIWwYuulWJQoNmZJS = -2.691507E-11F;
+		byte AsxQSLAVHfcNwO = 157;
+		byte GcZMdKhEfHTDbRBoKJzlqXKICl = 50;
+		byte hDmkPIR = 132;
+		float IFEQIfnybcimaxLfuBbDQ = -1.225362E-09F;
+		int wiTzdAySChdmLam = 74;
+		if (wiTzdAySChdmLam == 240109) {
+			wiTzdAySChdmLam = 828195;
+		}long ORUBxtJhHdIqsQfnpLKM = 31878267502747105;
+		short DctL = 19099;
+		short ACHyOhhoZtQiDtTfNiO = -11741;
+		int RcAsmnzijmPBtQIIYeP = 556309;
+		if (RcAsmnzijmPBtQIIYeP == 912944) {
+			RcAsmnzijmPBtQIIYeP = RcAsmnzijmPBtQIIYeP + 354558;
+		}int qfsEbN = 871156061;
+		if (qfsEbN == 137781) {
+			qfsEbN = 740809;
+		}double qmMVVOlgqQmkfOlVlxjzdXxYodh = 0.06969267;
+		double BFxMqAQhlUdJBSLI = 2.552839E-11;
+		short EZHgIkRRkODCEeTKBwLkjqDKOWSj = 25846;
+		long tHXJTJTqTNHAZidJHLqpaq = 28259765925621037;
+
+	}void LGFITPWDwTfFMHcLcczDAshTUcBRo() {
+		byte eFMGKwJLJjQUSswYz = 158;
+		int UeZdmnlLVTu = 56;
+		if (UeZdmnlLVTu == 332385) {
+			UeZdmnlLVTu = 390978;
+		}long UUKWtYjHKXqtnpbqsbcR = 28750949869603187;
+		double zsdkiEySzTQGdAddAxoy = 10359.32;
+		byte WlCtkkiAczuMoVTqtDzUuZPOW = 254;
+		long UqtPACGcMbMFxqlmakVWPdnwnhyX = 8989692930886210;
+		short mOpRFsRTzdNDNLexgQc = 30506;
+		double dJGXFBNkMBhx = 7.446145E-28;
+		byte BLVWPgu = 104;
+		float LARzBWgSkIDlmTjLkAoowPxQkgnqA = -1.042539E+11F;
+		double MnKPxW = 5.237665E+08;
+		float iXxlhgszxnjZQpLxjYA = -5.247039E+08F;
+		byte mShFZLREELGO = 186;
+		long QwummtPzqpddCPtVSF = 7080022586930642;
+		double AVmbwWzUlElCqUMqxWutAVsRMm = -4976.108;
+		long oxkeezyS = 28603107692737659;
+		long iyyiY = 80495566257529059;
+		short leoNgRQMsMDHRxIWpjsLmeoWMBIM = 6728;
+		double oeQLpmwKhPtbHiegCBm = -8.152171E+10;
+		short PthpOzQYMgaRf = -9267;
+
+	}void nlJVFgXoHSMYsuwidHHm() {
+		byte FwJzQJRw = 35;
+		long akyhSCmSe = 60721815240271540;
+		long ykLyMsiXal = 35546272207683248;
+		byte qUAXPgxh = 207;
+		int YHWVYielRed = 6623;
+		double VmBkxWHCzHjimiCT = -7.620518E-13;
+		int SITGsPNyxD = 89;
+		if (SITGsPNyxD == 328562) {
+			SITGsPNyxD = 193676;
+		}short fhJfhMBuQwiVSTzEtOFlTHFF = 32161;
+		float XNAhFpDeRJzoYfwxGFUA = 1.628986E-37F;
+		int ZHWVzEef = 7210558;
+		if (ZHWVzEef == 315038) {
+			ZHWVzEef = 263814;
+		}long ZQznFeaSsHebfGjIh = 77936005040720394;
+		int jYfkgPdUdAHGAQMQllkIZZtE = 16105213;
+		if (jYfkgPdUdAHGAQMQllkIZZtE == 191060) {
+			jYfkgPdUdAHGAQMQllkIZZtE = 232008;
+		}byte quhKtfDNSQaNOAdjmjBscF = 204;
+		int DBYlzKRxVneIbyfJzQj = 625882036;
+		if (DBYlzKRxVneIbyfJzQj == 100983) {
+			DBYlzKRxVneIbyfJzQj += 375229;
+		}long MKKNFXeRGFCtfjpTkYDkNuJFOGlYN = 70957376628934156;
+		double KXGBeWydPpVagRgm = -5.056575E-37;
+		double cYIlXoTCg = 1.576583E+28;
+		double qNgmNSLelOzCtZbO = 6.781803E-23;
+		byte UemKSZQDdgQPn = 212;
+		byte UfdFRTstmNa = 170;
+		float gXXaFFASNS = -25620.68F;
+
+	}
